622de84bb3bed38a69e1825c6ee788d24d524ae1
Brandon <102290201+icyde@users.noreply.github.com>
2023-09-13 11:15:27 +0800

    init repo

---
+++ app/ChatWindow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChatWindow = () => {
+  return (
+    <div>
+    hi
+      
+    </div>
+  );
+}
+
+export default ChatWindow;
---
+++ app/mockMessages.js
@@ -0,0 +1,7 @@
+export const mockMessages = [
+  {content:"lorem ipsum 1"},
+  {content:"lorem ipsum 2"},
+  {content:"lorem ipsum 3"}
+]
+
+export default mockMessages;
