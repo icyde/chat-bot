928e719ac2af740fda542846ab3cebcf005bd8e0
Brandon <102290201+icyde@users.noreply.github.com>
2023-09-13 12:36:01 +0800

    add box

--- app/ChatWindow.js
+++ app/ChatWindow.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 const ChatWindow = () => {
   return (
-    <div>
-    hi
-      
-    </div>
+    <div className="fixed z-90 bottom-[84px]
+     right-8 w-[405px] h-[650px] bg-[#F4F5F7]">Oyika</div>
   );
 }
 
