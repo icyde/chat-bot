682b0236568cc0a1bbb3305f3bdaeab1762088a6
Brandon <102290201+icyde@users.noreply.github.com>
2023-09-15 22:22:15 +0800

    fix: add onclick test...

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,20 @@ import { useState } from "react";
 
 
 export default function Home() {
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
   const [messages, setMessages] = useState([]);
 
   const handleButton = () => {
+    showModal ? hide() : show();
     setShowModal(!showModal);
+  };
+
+  const hide = () => {
+    window.parent.postMessage("hide", "*");
+  };
+  
+  const show = () => {
+    window.parent.postMessage("show", "*");
   };
 
 
@@ -24,12 +33,19 @@ export default function Home() {
       <button
         onClick={handleButton}
         id="chatbot-button"
-        className={`bg-[#2d549f] hover:bg-blue-600 font-bold rounded-full ${!showModal ? "p-2" : "p-3"} fixed z-90 bottom-10 right-8`}
+        className={`bg-[#2d549f] hover:bg-blue-600 font-bold rounded-full ${
+          !showModal ? "p-2" : "p-3"
+        } fixed z-90 bottom-10 right-8`}
       >
         {!showModal ? (
           <Image src="/chat.png" alt="chat icon" width={25} height={25}></Image>
         ) : (
-          <Image src="/close.png" alt="close icon" width={15} height={15}></Image>
+          <Image
+            src="/close.png"
+            alt="close icon"
+            width={15}
+            height={15}
+          ></Image>
         )}
       </button>
     </div>
