3555a09eeeaa25a3ed13cb28714be25305e3b654
Brandon <102290201+icyde@users.noreply.github.com>
2023-10-26 16:40:56 +0800

    remove fastapi

--- api/config.py
+++
@@ -1,7 +0,0 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-class Settings(BaseSettings):
-    zendesk_host: str = "https://oyika179.zendesk.com/sc"
-    zendesk_key_id: str = "app_65368d972be3d115294be513"
-    zendesk_secret_key: str = "mIIDlC5TYBF0ZkVtkDTt4xJOdZzB7-6baoppJCb4MLDg_zWwjb_RDVzXBF22KibP4F3L6wb4EnCO0rlRpW_3Bg"
-    zendesk_app_id: str = "653630f5da6a21b7e5116952"
